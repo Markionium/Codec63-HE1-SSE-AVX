0d4d017fed31bb85b907e8f6e9fd5901ba1fe84a
Preben N. Olsen <preben@simula.no>
2013-08-27 12:53:00 +0200

    dump_image: cleaned up

--- common.c
+++ common.c
@@ -149,8 +149,7 @@ struct frame* create_frame(struct c63_common *cm, yuv_t *image)
 
 void dump_image(yuv_t *image, int w, int h, FILE *fp)
 {
-    fwrite(image->Y, 1, w*h, fp);
-    fwrite(image->U, 1, w*h/4, fp);
-    fwrite(image->V, 1, w*h/4, fp);
+  fwrite(image->Y, 1, w*h, fp);
+  fwrite(image->U, 1, w*h/4, fp);
+  fwrite(image->V, 1, w*h/4, fp);
 }
-
