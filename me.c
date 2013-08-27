e3440a43f6ac6c8d686f8e1633c415584d53c7c5
Preben N. Olsen <preben@simula.no>
2013-08-27 14:15:35 +0200

    me: sorted headers

--- me.c
+++ me.c
@@ -1,15 +1,14 @@
+#include <assert.h>
+#include <errno.h>
+#include <getopt.h>
+#include <limits.h>
+#include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
-#include <errno.h>
-#include <stdint.h>
-#include <math.h>
-#include <assert.h>
-#include <limits.h>
 
 #include "c63.h"
-
 
 /* Motion estimation for 8x8 block */
 static void me_block_8x8(struct c63_common *cm, int mb_x, int mb_y, uint8_t *orig, uint8_t *ref, int cc)
