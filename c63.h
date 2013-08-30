87b86d17fc0809c2c87927d0c2c76535b04113c7
Preben N. Olsen <preben@simula.no>
2013-08-30 12:46:27 +0200

    c63 function decls: added comments

--- c63.h
+++ c63.h
@@ -99,31 +99,36 @@ struct c63_common
     struct entropy_ctx e_ctx;
 };
 
-void put_bytes(FILE *fp, const void* data, unsigned int len);
-void put_byte(FILE *fp, int byte);
-void put_bits(struct entropy_ctx *c, uint16_t bits, uint8_t n);
-void flush_bits(struct entropy_ctx *c);
-uint8_t get_byte(FILE *fp);
+/* Definitions are found in 'io.c' */
 int read_bytes(FILE *fp, void *data, unsigned int sz);
 uint16_t get_bits(struct entropy_ctx *c, uint8_t n);
+uint8_t get_byte(FILE *fp);
+void flush_bits(struct entropy_ctx *c);
+void put_bits(struct entropy_ctx *c, uint16_t bits, uint8_t n);
+void put_byte(FILE *fp, int byte);
+void put_bytes(FILE *fp, const void* data, unsigned int len);
 
+/* Definitions are found in 'dsp.c' */
 void dct_quant_block_8x8(int16_t *in_data, int16_t *out_data,
     uint8_t *quant_tbl);
 void dequant_idct_block_8x8(int16_t *in_data, int16_t *out_data,
     uint8_t *quant_tbl);
 void sad_block_8x8(uint8_t *block1, uint8_t *block2, int stride, int *result);
 
+/* Definition found in 'c63_write.c' */
 void write_frame(struct c63_common *cm);
-void dequantize_idct(int16_t *in_data, uint8_t *prediction, uint32_t width,
-    uint32_t height, uint8_t *out_data, uint8_t *quantization);
+
+/* Definitions are found in 'common.c' */
+struct frame* create_frame(struct c63_common *cm, yuv_t *image);
 void dct_quantize(uint8_t *in_data, uint8_t *prediction, uint32_t width,
     uint32_t height, int16_t *out_data, uint8_t *quantization);
-
+void dequantize_idct(int16_t *in_data, uint8_t *prediction, uint32_t width,
+    uint32_t height, uint8_t *out_data, uint8_t *quantization);
 void destroy_frame(struct frame *f);
-struct frame* create_frame(struct c63_common *cm, yuv_t *image);
+void dump_image(yuv_t *image, int w, int h, FILE *fp);
+
+/* Definitions are found in 'me.c' */
 void c63_motion_estimate(struct c63_common *cm);
 void c63_motion_compensate(struct c63_common *cm);
-
-void dump_image(yuv_t *image, int w, int h, FILE *fp);
 
 #endif /* CPU_MJPEG_H */
